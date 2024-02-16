1427b8d478d693cfa8e08ef7c2295013cabc33ba
Kuda Mbata <kudambata2005@gmail.com>
2024-02-16 15:06:56 +0200

    basic components and structure coming together

--- components/header.js
+++ components/header.js
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import Nav from "./nav";
+
 export default function Header() {
     return (
-        <></>
+        <header className="w-full">
+            <Image 
+            src="/logo.png"
+            alt="logo"
+            width={100}
+            height={100}
+            quality={90}
+            />
+            <Nav/>
+        </header>
+        
     );
 }
--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,20 @@
 
 
 export default function Nav() {
-
+    return (
+        <ul className="flex flex-row">
+            <li className="p-3">
+                <a href="#about">About</a>
+            </li>
+            <li className="p-3">
+                <a href="#experience">Experience</a>
+            </li>
+            <li className="p-3">
+                <a href="#work">Work</a>
+            </li>
+            <li className="p-3">
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    )
 }
